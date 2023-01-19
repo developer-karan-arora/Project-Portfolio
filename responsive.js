abebde679bb4cb31f0f2423e6de449de4ec940c4
Developer-karan-projects <karanarora200229@gmail.com>
2023-01-20 02:28:31 +0530

    debug menu js

--- responsive.js
+++ responsive.js
@@ -1,22 +1,4 @@
-function clodeMenuOnClickList() {
-    console.log("first");
-    document.getElementsByTagName("aside")[0].style.transform =
-      "translateX(-300px)";
-  }
-  function close() {
-    console.log("closing");
-    document.getElementsByTagName("aside")[0].style.transform =
-      "translateX(-300px)";
-  }
-  let openIcon = document.getElementById("aside-open");
-  openIcon.addEventListener("click", () => {
-    document.getElementsByTagName("aside")[0].style.transform =
-      "translateX(0px)";
-  });
-  let closeIcon = document.getElementById("aside-close");
-  closeIcon.addEventListener("click", () => {
-    close();
-  });
+
   // let asideMenu = document.querySelectorAll(".aside-menu");
   // asideMenu.forEach((item) => {
   //   item.addEventListener("click", close());
